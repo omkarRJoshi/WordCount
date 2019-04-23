3b3048e4b7696e275eccd1f30553f40f4c28e648
omkarRJoshi <omkarjoshi9545@gmail.com>
2019-04-23 21:40:57 +0530

    wordcount program

---
+++ src/com/practice/wordcount/WordCount.scala
@@ -0,0 +1,20 @@
+package com.practice.wordcount
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object WordCount {
+  def main(args : Array[String]){
+    val sparkConf = new SparkConf() //
+    //now setting app name and master
+    sparkConf.setAppName("wordCountProgram").setMaster("local[*]") // local[*] Run Spark locally with as many worker threads as logical cores on your machine.
+    
+    val sparkContext = new SparkContext(sparkConf);
+    val file = sparkContext.textFile("/home/omkar/workspace/sparkWorkspace/WordCount/inputFile") //path of input file
+    val words = file.flatMap(_.split(" ")) //splits each token seperated by " "
+    val assignOne = words.map((_,1))
+    val count = assignOne.reduceByKey(_ + _)
+    count.collect().foreach(println)
+    
+  }
+}
